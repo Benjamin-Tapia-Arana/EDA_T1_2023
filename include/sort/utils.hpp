f9750c3e4a2cb0677455754de37033aa136683c6
Benjamin-Tapia-Arana <batapia1@miuandes.cl>
2023-09-04 01:44:11 -0300

    unused utils.hpp functions

--- include/sort/utils.hpp
+++ include/sort/utils.hpp
@@ -3,16 +3,9 @@
 
 namespace sort{
 	
-	int* createArray(int n);
-	int* createRandomArray(int n);
-	int* createRandomIntArray(int n, int minVal = 0, int maxVal = 100);
-	void deleteArray(int* A);
 	void printArray(int* A, int n);
-	void setAll(float* A, int n, float value);
 	void swap(int* A, int i, int j);
 	int getRandomInt(int min, int max);
-	int* linspace(int max, int n_parts);
-	int maxLengthNumber(int* A, int n);
 	int maxLength(int* A, int n);
 }
 
