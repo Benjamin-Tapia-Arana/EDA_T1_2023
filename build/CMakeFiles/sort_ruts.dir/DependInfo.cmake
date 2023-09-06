923979bfa95e5714688999cd0c32eb1df62a345f
Taspee <tfaspee@miuandes.cl>
2023-09-05 22:10:33 -0300

    holas

---
+++ build/CMakeFiles/sort_ruts.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/edauandes/EDA_T1_2023/main/sort_ruts.cpp" "CMakeFiles/sort_ruts.dir/main/sort_ruts.cpp.o" "gcc" "CMakeFiles/sort_ruts.dir/main/sort_ruts.cpp.o.d"
+  "/home/edauandes/EDA_T1_2023/src/sort.cpp" "CMakeFiles/sort_ruts.dir/src/sort.cpp.o" "gcc" "CMakeFiles/sort_ruts.dir/src/sort.cpp.o.d"
+  "/home/edauandes/EDA_T1_2023/src/utils.cpp" "CMakeFiles/sort_ruts.dir/src/utils.cpp.o" "gcc" "CMakeFiles/sort_ruts.dir/src/utils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
